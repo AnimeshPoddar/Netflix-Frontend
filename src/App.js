3b13def95fd15cabd851229d4f9ad7fcdd319d11
Animesh <animesh.poddar@vkaps.com>
2022-12-06 16:39:54 +0530

    Initial users preview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
+  const [users, setUsers] = useState([]);
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzg1ZTM5ZGQxNmExN2QyN2VlZmY5M2UiLCJpYXQiOjE2NzAzMjMxODJ9.q2yU8ZFCacyciYgVGqYDVa66eck0vWZMidK59oRz9Ro";
+  const fetchAllUsers = async () => {
+    try {
+      const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = res.data.users;
+      console.log("data=>", data);
+      setUsers(data);
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+
   return (
     <>
       <h1>Welcome to the frontend of netflix dummy.</h1>
+      {users.map((user) => {
+        return <h1>{user.username}</h1>;
+      })}
     </>
   );
 }
